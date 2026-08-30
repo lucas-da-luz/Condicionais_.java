b63a3cd99b02eb9f77a38c66c3c239f59f9d76a5
Lucas da Luz Vitória <89362398+lucas-da-luz@users.noreply.github.com>
2026-08-29 22:45:56 -0300

    Create imc.java

---
+++ imc.java
@@ -0,0 +1,34 @@
+package Anexo_7;
+
+import java.util.Scanner;
+
+public class imc_condicional {
+    static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double imc, peso, altura;
+        String nome;
+        String seuIMC = "Seu imc está: ";
+        System.out.println("Insira seu nome:");
+        nome = scan.nextLine();
+
+        System.out.println("insira seu peso:");
+        peso = scan.nextDouble();
+
+        System.out.println("insira sua altura:");
+        altura = scan.nextDouble();
+
+        imc = peso/altura;
+
+        if (20 > imc) {
+            System.out.println(seuIMC + "Abaixo");
+        } else if ( 25 > imc){
+            System.out.println(seuIMC + "Normal");
+        } else if ( 30 > imc){
+        System.out.println(seuIMC + "Acima do peso");
+        } else if ( 40 > imc){
+        System.out.println(seuIMC + "Obeso");
+        } else {
+        System.out.println(seuIMC + "Obeso mórbido");
+        }
+    }
+}
